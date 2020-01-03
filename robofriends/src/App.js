91c53a866917d4b718d114c2b293c2d595f42231
kddaniel8000 <danielwebdeveloper220@gmail.com>
2020-01-03 15:45:16 -0500

    Creating CardList component

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -2,18 +2,14 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-import Card from './components/Card/Card';
+import CardList from './components/CardList/CardList';
 import { robots } from './components/Robots/Robots';
 
 class App extends Component {
   render(){
     return (
       <div>
-        <Card 
-          key={robots[0].id}
-          name={robots[0].name}
-          email={robots[0].email}
-        />
+        <CardList robots = {robots} />
       </div>
     )
   }
