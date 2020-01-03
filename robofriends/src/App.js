b288e46a806f5e808e73bfa807c0172a118a088e
kddaniel8000 <danielwebdeveloper220@gmail.com>
2020-01-03 16:51:02 -0500

    SearchBox and CardList updated to take props from App to change state

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -3,13 +3,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 import CardList from './components/CardList/CardList';
+import SearchBox from './components/SearchBox/SearchBox';
+
 import { robots } from './components/Robots/Robots';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: '',
+    }
+  }
+
+  onSearchChange = event => {
+    // console.log(event.target.value);
+    this.setState({
+      searchfield: event.target.value
+    })
+  }
+
   render(){
+
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(
+        this.state.searchfield.toLowerCase()
+      )
+    })
+
     return (
-      <div>
-        <CardList robots = {robots} />
+      <div className="tc">
+        <h1 className='f1'>Robofriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots = {filteredRobots} />
       </div>
     )
   }
