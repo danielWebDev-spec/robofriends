908c84326b2d079faa84539e6d043a0e2d982d13
deelocruz <kddaniel8000@gmail.com>
2020-01-27 04:39:27 -0500

    redux store and Provider

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { searchRobots } from './reducers/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
