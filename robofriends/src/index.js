b42b55f2fff144361c0e412a95bc42b5abca1f7d
deelocruz <kddaniel8000@gmail.com>
2020-01-27 21:03:18 -0500

    adding redux async actions

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { searchRobots } from './reducers/reducers';
+import { requestRobots, searchRobots } from "./reducers/reducers";
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+
+const rootReducer = combineReducers({
+  requestRobots, searchRobots
+})
+const store = 
+  createStore(rootReducer, applyMiddleware(
+    thunk,
+    logger
+  ));
 
 ReactDOM.render(
     <Provider store={store}>
